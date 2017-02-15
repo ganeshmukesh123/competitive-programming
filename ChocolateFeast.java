378e39337e4e417b75824e7cd4364c303f7715f0
ganeshmukesh123 <ganeshmukesh123@gmail.com>
2017-02-15 22:57:54 +0530

    Create ChocolateFeast.java

---
+++ ChocolateFeast.java
@@ -0,0 +1,28 @@
+//problem statement https://www.hackerrank.com/challenges/chocolate-feast
+import java.io.*;
+import java.util.*;
+
+public class ChocolateFeast {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int n,c,m;
+        while(t>0){
+            n=sc.nextInt();
+            c=sc.nextInt();
+            m=sc.nextInt();
+            int ch,wr;
+            ch=n/c;
+            wr=ch;
+            while(wr>=m){
+                wr=wr-m;
+                ch++;
+                wr++;
+            }
+            System.out.println(ch);
+            t--;
+        }
+    }
+}
